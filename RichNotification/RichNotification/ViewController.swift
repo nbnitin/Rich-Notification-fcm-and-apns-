625991e64e61d8cb6806e9adcc97c002b375c245
Nitin <nbnitin.bhatia@gmail.com>
2018-01-08 18:08:42 +0530

    adding project

---
+++ RichNotification/RichNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RichNotification
+//
+//  Created by Nitin Bhatia on 08/01/18.
+//  Copyright © 2018 Nitin Bhatia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
